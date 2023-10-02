088ce70370faa6b630bdad4acf643e4fcc88ed31
Karen Rodriguez <kyrodriguez@gmail.com>
2023-10-02 15:27:46 -0500

    Se agrego modal y editor en el formulario

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import './assets/main.css'
+import './assets/main.css';
+import './assets/ckedoctor-reset.css';
+import CKEditor from '@ckeditor/ckeditor5-vue';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import FullCalendar from '@fullcalendar/vue3'; // Importa FullCalendar
+import dayGridPlugin from '@fullcalendar/daygrid'; // Importa el complemento de DayGrid
+import interactionPlugin from '@fullcalendar/interaction'; // Importa el complemento de Interaction
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(FullCalendar);
+app.use(CKEditor); // Usa FullCalendar como un complemento global
 
 import App from './App.vue'
 import router from './router'
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-gray-50">
+    <!-- <section class="bg-gray-50">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -14,7 +14,7 @@
                             <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
                           </svg>
                         </span>
-                      </div>
+                    </div>
                     <form class="space-y-4 md:space-y-6" @submit.prevent="login">
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">
@@ -25,7 +25,7 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                 placeholder="name@company.com" required />
                             <div id="email-help" class="text-sm text-gray-500">
-                                Ayuda opcional para el correo electrónico.
+                                Ingresa tu correo electrónico
                             </div>
                         </div>
                         <div>
@@ -37,7 +37,7 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                 required />
                             <div id="password-help" class="text-sm text-gray-500">
-                                Ayuda opcional para la contraseña.
+                                Ingresa tu contraseña.
                             </div>
                         </div>
                         <button type="submit"
@@ -48,7 +48,67 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
+    <!-- component -->
+    <div class="bg-gray-800 h-screen">
+        <div class="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
+            <div class="flex flex-col sm:flex-row rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 bg-white sm:mx-0">
+                <div class="flex flex-col w-full md:w-1/2 p-4">
+                    <div class="flex flex-col flex-1 justify-center mb-8">
+                        <h1 class="text-4xl leading-tight tracking-tight text-gray-900 text-center font-thin">
+                            Acceda a su cuenta
+                        </h1>
+    
+                        <div class="w-full mt-4">
+                        <div v-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                            <span class="block sm:inline">{{ error }}</span>
+                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                              <svg class="fill-current h-6 w-6 text-red-500 cursor-pointer" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" @click="closeAlert">
+                                <title>Cerrar</title>
+                                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+                              </svg>
+                            </span>
+                        </div>
+                        <form class="form-horizontal w-3/4 mx-auto" @submit.prevent="login">
+                            <div class="py-4">
+                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900">
+                                    Correo electrónico
+                                </label>
+                                <input v-model="email" type="email" name="email" id="email" aria-label="Correo electrónico"
+                                    aria-describedby="email-help"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="name@company.com" required />
+                                <div id="email-help" class="text-sm text-gray-500">
+                                    Ingresa tu correo electrónico
+                                </div>
+                            </div>
+                            <div class="py-4">
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900">
+                                    Contraseña
+                                </label>
+                                <input v-model="password" type="password" name="password" id="password" aria-label="Contraseña"
+                                    aria-describedby="password-help" placeholder="••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    required />
+                                <div id="password-help" class="text-sm text-gray-500">
+                                    Ingresa tu contraseña.
+                                </div>
+                            </div>
+                            <div class="py-2">
+                                <button type="submit"
+                                class="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                Iniciar sesión
+                                </button>
+                            </div>
+                      
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="hidden md:block md:w-1/2 rounded-r-lg" style="background: url('/img/img_login.jpg'); background-size: cover; background-position: center center;"></div>         
+        </div>
+    </div>
+</div>
 </template>
   
 <script setup>
@@ -74,7 +134,10 @@ const login = async () => {
     const response = await axios.post(`${dataApi}/login`, formData);
 
     // Si la autenticación es exitosa, almacenar el token en localStorage
-    localStorage.setItem('authToken', response.data.token); // Supongo que response.data.token contiene el token
+    localStorage.setItem('authToken', response.data);// Supongo que response.data.token contiene el token
+    localStorage.setItem('api_token', response.data.api_token);// Supongo que response.data.token contiene el token
+    localStorage.setItem('nameUser',  response.data.name); 
+    localStorage.setItem('emailUser', response.data.email);
 
     // Redirigir al usuario al área de administración
     router.push({ name: 'admin' });
